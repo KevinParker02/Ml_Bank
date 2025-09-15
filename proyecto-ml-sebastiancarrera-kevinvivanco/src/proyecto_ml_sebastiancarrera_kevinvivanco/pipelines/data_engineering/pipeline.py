1e856ab2ac4628b4c5fa08480c0e4ba282a07f24
Sebastian <sebastiancarrera2021@gmail.com>
2025-09-15 20:45:40 -0300

    Fix secuencia de kedro run
    Se arreglo el orden secuencial de kedro run

--- proyecto-ml-sebastiancarrera-kevinvivanco/src/proyecto_ml_sebastiancarrera_kevinvivanco/pipelines/data_engineering/pipeline.py
+++ proyecto-ml-sebastiancarrera-kevinvivanco/src/proyecto_ml_sebastiancarrera_kevinvivanco/pipelines/data_engineering/pipeline.py
@@ -8,18 +8,23 @@ from .nodes import *
 
 def create_pipeline(**kwargs):
     return Pipeline([
+        # 1) Limpieza inicial -> NIV
         node(
             func=clean_datasets,
             inputs=["cleaned_dataset","customer_agg","RFM"],
             outputs=["NIV_cleaned_dataset","NIV_customer_agg","NIV_RFM"],
             name="clean_datasets_node"
         ),
+
+        # 2) Transformación -> PRI
         node(
             func=transform_datasets,
-            inputs=["cleaned_dataset","customer_agg","RFM"],
+            inputs=["NIV_cleaned_dataset","NIV_customer_agg","NIV_RFM"],
             outputs=["PRI_cleaned","PRI_customer","PRI_rfm"],
             name="transform_datasets_node"
         ),
+
+        # 3) Integración -> PRI_full
         node(
             func=integrate_datasets,
             inputs=["PRI_cleaned","PRI_customer","PRI_rfm"],
@@ -27,4 +32,3 @@ def create_pipeline(**kwargs):
             name="integrate_datasets_node"
         )
     ])
-
