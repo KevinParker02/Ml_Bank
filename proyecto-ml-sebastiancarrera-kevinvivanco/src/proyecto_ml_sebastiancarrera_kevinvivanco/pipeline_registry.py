e52d5f6533e6892944d4abb4d36133118911c34d
KevinVivanco_02 <131885520+KevinParker02@users.noreply.github.com>
2025-10-24 16:04:34 -0300

    Base para seguir con los pipelines

--- proyecto-ml-sebastiancarrera-kevinvivanco/src/proyecto_ml_sebastiancarrera_kevinvivanco/pipeline_registry.py
+++ proyecto-ml-sebastiancarrera-kevinvivanco/src/proyecto_ml_sebastiancarrera_kevinvivanco/pipeline_registry.py
@@ -4,16 +4,16 @@
 from typing import Dict
 from kedro.pipeline import Pipeline
 from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import data_engineering, reporting
-from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import feature_engineering as fe
+#from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import feature_engineering as fe
 
 def register_pipelines() -> Dict[str, Pipeline]:
     data_engineering_pipeline = data_engineering.create_pipeline()
     reporting_pipeline = reporting.create_pipeline()
-    feature_engineering_pipeline = fe.create_pipeline()
+    #feature_engineering_pipeline = fe.create_pipeline()
 
     return {
         "data_engineering": data_engineering_pipeline,
         "reporting": reporting_pipeline,
-        "feature_engineering": feature_engineering_pipeline,
-        "__default__": data_engineering_pipeline + feature_engineering_pipeline + reporting_pipeline,  # corre si pones `kedro run`
+        #"feature_engineering": feature_engineering_pipeline,
+        "__default__": data_engineering_pipeline + reporting_pipeline,  # corre si pones `kedro run`
     }
