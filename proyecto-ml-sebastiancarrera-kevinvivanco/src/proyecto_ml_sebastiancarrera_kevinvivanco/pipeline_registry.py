dae2dde3973ccedfc8bdad1e47833cfbd60b45bb
Sebastian <sebastiancarrera2021@gmail.com>
2025-11-27 19:07:43 -0300

    Pipeline nuevo 🍐

--- proyecto-ml-sebastiancarrera-kevinvivanco/src/proyecto_ml_sebastiancarrera_kevinvivanco/pipeline_registry.py
+++ proyecto-ml-sebastiancarrera-kevinvivanco/src/proyecto_ml_sebastiancarrera_kevinvivanco/pipeline_registry.py
@@ -8,6 +8,7 @@ from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import feature_engineer
 from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import feature_selection as fs
 from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import classification_models as cm
 from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import regression_models as rm
+from proyecto_ml_sebastiancarrera_kevinvivanco.pipelines import unsupervised_learning as ul
 def register_pipelines() -> Dict[str, Pipeline]:
     data_engineering_pipeline = data_engineering.create_pipeline()
     reporting_pipeline = reporting.create_pipeline()
@@ -15,6 +16,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     feature_selection_pipeline = fs.create_pipeline()
     classification_models_pipeline = cm.create_pipeline()
     regression_models_pipeline = rm.create_pipeline()
+    unsupervised_learning_pipeline= ul.create_pipeline()
 
     return {
         "data_engineering": data_engineering_pipeline,
@@ -22,6 +24,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "feature_selection": feature_selection_pipeline,
         "classification_models": classification_models_pipeline,
         "regression_models": regression_models_pipeline,
+        "unsupervised-learning": unsupervised_learning_pipeline,
         "reporting": reporting_pipeline,
-        "__default__": data_engineering_pipeline + feature_engineering_pipeline + feature_selection_pipeline + classification_models_pipeline + regression_models_pipeline + reporting_pipeline  # corre si pones `kedro run`
+        "__default__": data_engineering_pipeline + feature_engineering_pipeline + feature_selection_pipeline + classification_models_pipeline + regression_models_pipeline + unsupervised_learning_pipeline + reporting_pipeline  # corre si pones `kedro run`
     }
---
+++ proyecto-ml-sebastiancarrera-kevinvivanco/src/proyecto_ml_sebastiancarrera_kevinvivanco/pipelines/unsupervised_learning/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'unsupervised_learning'
+generated using Kedro 1.0.0
+"""
